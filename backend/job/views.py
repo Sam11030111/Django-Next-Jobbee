9dd5fcd8dd3ed30f4b55e77c842a1e99ccd5854f
Sam11030111 <sam86110306@gmail.com>
2024-09-07 12:39:19 -0500

    Create api for job

--- backend/job/views.py
+++ backend/job/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from django.db.models import Avg, Min, Max, Count
 
 from .serializer import JobSerializer
 from .models import Job
+
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
@@ -14,3 +18,72 @@ def getAllJobs(request):
     serilaizer = JobSerializer(jobs, many=True)
 
     return Response(serilaizer.data)
+
+
+@api_view(['GET'])
+def getJob(request, pk):
+    job = get_object_or_404(Job, id=pk)
+    serilaizer = JobSerializer(job, many=False)
+
+    return Response(serilaizer.data)
+
+
+@api_view(['POST'])
+def createNewJob(request):
+    data = request.data
+    job = Job.objects.create(**data)
+    serilaizer = JobSerializer(job, many=False)
+
+    return Response(serilaizer.data)
+
+
+@api_view(['PUT'])
+def updateJob(request, pk):
+    job = get_object_or_404(Job, id=pk)
+
+    job.title = request.data['title']
+    job.description = request.data['description']
+    job.email = request.data['email']
+    job.address = request.data['address']
+    job.jobType = request.data['jobType']
+    job.education = request.data['education']
+    job.industry = request.data['industry']
+    job.experience = request.data['experience']
+    job.salary = request.data['salary']
+    job.positions = request.data['positions']
+    job.company = request.data['company']
+
+    job.save()
+
+    serializer = JobSerializer(job, many=False)
+
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def deleteJob(request, pk):
+    job = get_object_or_404(Job, id=pk)
+
+    job.delete()
+
+    return Response({'message': 'Job is Deleted.'}, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+def getTopicStats(request, topic):
+    # args = {'title__icontains': topic}
+    # jobs = Job.objects.filter(**args)
+    jobs = Job.objects.filter(title__icontains=topic)
+
+    if len(jobs) == 0:
+        return Response({'messgae': 'No stats found for {topic}'.format(topic=topic)})
+
+    stats = jobs.aggregate(
+        total_jobs=Count('title'),
+        avg_positions=Avg('positions'),
+        avg_salary=Avg('salary'),
+        min_salary=Min('salary'),
+        max_salary=Max('salary')
+    )
+
+    return Response(stats)
